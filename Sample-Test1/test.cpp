f7b96c7fb89cf70530932f9640e3be9356c3830d
kkh <dhadgfl@gmail.com>
2023-06-01 14:18:35 +0900

    Revert "additional commit"
    This reverts commit 2f36264c70d93961ddf0176f180fdfc261f3b2f1.

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,7 +3,7 @@
 
 class AccountFixture : public testing::Test {
 public:
-	Account account{ 10000, 5 };
+	Account account{ 10000 };
 };
 
 TEST_F(AccountFixture, CreateAccountInit10000won) {
@@ -18,22 +18,4 @@ TEST_F(AccountFixture, Deposit) {
 TEST_F(AccountFixture, Withdraw) {
 	account.Withdraw(600);
 	EXPECT_EQ(9400, account.getBalance());
-}
-
-TEST_F(AccountFixture, CompoundInterest) {
-	account.CompoundInterest();
-	EXPECT_EQ(10500, account.getBalance());
-	account.CompoundInterest();
-	EXPECT_EQ(11025, account.getBalance());
-}
-
-TEST_F(AccountFixture, InterestPercentage) {
-	account.setInterestPercentage(8);
-	EXPECT_EQ(8, account.getInterestPercentage());
-}
-
-TEST_F(AccountFixture, CompoundInterestAfterNyears) {
-	account.setInterestPercentage(10);
-	account.CompoundInterestAfterNyears(3);
-	EXPECT_EQ(13310, account.getBalance());
 }
--- cra_accout_kkh/cal.cpp
+++ cra_accout_kkh/cal.cpp
@@ -1,7 +1,7 @@
 class Account {
 public:
-	explicit Account(int balance, int interestPercentage)
-		: balance(balance), interestPercentage(interestPercentage) {
+	explicit Account(int balance)
+		: balance(balance) {
 	}
 
 	int getBalance() {
@@ -16,27 +16,6 @@ public:
 		balance -= money;
 	}
 
-	void CompoundInterest() {
-		balance *= (100 + interestPercentage);
-		balance /= 100;
-	}
-
-	void setInterestPercentage(int interestPercent) {
-		interestPercentage = interestPercent;
-	}
-
-	int getInterestPercentage() {
-		return interestPercentage;
-	}
-
-	void CompoundInterestAfterNyears(int years) {
-		for (int i = 0; i < years; i++) {
-			balance *= (100 + interestPercentage);
-			balance /= 100;
-		}
-	}
-
 private:
 	int balance;
-	int interestPercentage;
 };
